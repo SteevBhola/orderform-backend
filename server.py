086e6ea28f02f66a9e0c32cd2bb6903e6252af37
SteevBhola <steevbhola68@gmail.com>
2025-05-02 22:48:47 +0530

    Update server.py

--- server.py
+++ server.py
@@ -89,8 +89,9 @@ def submit_order():
         msg.add_attachment(f.read(), maintype="application", subtype="pdf", filename="Order.pdf")
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-    smtp.login(smtp_user, smtp_pass)
-    smtp.send_message(msg)
+        smtp.login(smtp_user, smtp_pass)
+        smtp.send_message(msg)
+
 
     os.remove(pdf_path)
     response = jsonify({"message": "✅ Order submitted and emailed to head office."})
